00ef2e615986bd4815dc4fe0e23f5e7bd4a4b725
kirube1992 <kirubelaynalem92@gmail.com>
2026-07-26 18:26:15 +0300

    complete project service

--- backend/src/main/java/com/bridgeos/backend/service/ProjectService.java
+++ backend/src/main/java/com/bridgeos/backend/service/ProjectService.java
@@ -20,16 +20,12 @@ public class ProjectService {
     private final ProjectRepository projectRepository;
     private final UserService  userService;
 
-
     @Transactional
     private  Project createProject(Project project, Long userId) {
 
         log.info("Creating a project {}", project.getName());
-
         User createdBy = userService.getUserById(userId);
-
         project.setCreatedBy(createdBy);
-
 
         return projectRepository.save(project);
 
@@ -38,17 +34,36 @@ public class ProjectService {
     public  List<Project> getAllProject() {
         log.info("fetch all project");
         return projectRepository.findAll();
-
     }
 
     public Project getProjectById(long id) {
-
-
        log.info("fetching user by id {}", id);
-
        return projectRepository.findById(id)
                .orElseThrow(() -> new RuntimeException("Project not found by id: " + id));
     }
 
+
+
+    @Transactional
+    public Project updatProject(long id, Project updateProject) {
+        log.info("Updating a project with id: {}", id);
+
+      Project exstingProject = getProjectById(id);
+      exstingProject.setName(updateProject.getName());
+      exstingProject.setDescription(updateProject.getDescription());
+      exstingProject.setClientContext(updateProject.getClientContext());
+      exstingProject.setStatus(updateProject.getStatus());
+      exstingProject.setDeadLine(updateProject.getDeadLine());
+      return  projectRepository.save(exstingProject);
+
+    }
+
+    public void deleteProject(long id) {
+        log.info("deleteing  a project with id: {}", id);
+
+        getProjectById(id);
+
+        projectRepository.deleteById(id);
+    }
 
 }
