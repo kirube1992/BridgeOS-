a5ee41d4d059eb3126a2a07202a2ae74d8fd5a10
kirube1992 <kirubelaynalem92@gmail.com>
2026-07-27 20:28:35 +0300

    add work item entity

---
+++ backend/src/main/java/com/bridgeos/backend/entity/WorkItem.java
@@ -0,0 +1,69 @@
+package com.bridgeos.backend.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.logging.log4j.util.Lazy;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "work_item")
+public class WorkItem {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(length = 500)
+    private String description;
+
+    @Column(name = "business_context_notes", length = 1000)
+    private String businessContextNotes;
+
+    @Column(name = "acceptance_criteria", length = 1000)
+    private String AcceptanceCriteria;
+
+    @Column(name= "clarity_score")
+    private Integer clarityScore;
+
+    @Column(name = "priority")
+    @Enumerated(EnumType.STRING)
+    private Priority priority = Priority.MEDIUM;
+
+    @Column(name = "work_item_status")
+    @Enumerated(EnumType.STRING)
+    private WorkItemStatus status = WorkItemStatus.TODO;
+
+    @Column(name="dead_line")
+    private LocalDateTime deadline;
+
+    @Column(name ="created_at")
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(name = "updated_at")
+    private  LocalDateTime updatedAt = LocalDateTime.now();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="project_id", nullable = false)
+    private Project project;
+
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="assigned_to", nullable = false)
+    private User assignedTo;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="created_by", nullable = false)
+    private User createdBy;
+}
