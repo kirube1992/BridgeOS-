24cb22d2628e1a86ff0df85501c83e0d60f11ccf
kirube1992 <kirubelaynalem92@gmail.com>
2026-08-08 22:26:19 +0300

    add audit&evnt repository

---
+++ backend/src/main/java/com/bridgeos/backend/repository/AuditEventRepository.java
@@ -0,0 +1,35 @@
+package com.bridgeos.backend.repository;
+
+
+import com.bridgeos.backend.entity.AuditEvent;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.lang.classfile.Interfaces;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface AuditEventRepository extends JpaRepository<AuditEvent, Long> {
+
+
+    List<AuditEvent> findByEntityTypeAndEntityId(String entityType, Long entityId);
+
+    List<AuditEvent> findByProjectID(Long projectiD);
+
+    List<AuditEvent> findByActorId(Long actorId);
+
+    @Query("SELECT a FROM AuditEvent a WHERE " +
+            "LOWER(a.summary) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+            "CAST(a.detail AS text) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    List<AuditEvent> searchByKeyword(String keyword);
+
+
+    List<AuditEvent> seracByKeyWord(String keyword);
+
+    List<AuditEvent>  findByProjectIdAndAtBetween(Long porjectId, LocalDateTime start, LocalDateTime end);
+
+
+
+}
